791da8a9e4d0ccfd0f8aab39bc7196b04e706e7d
Aidan Reidel <aidan.reidel@gmail.com>
2019-05-23 12:48:36 -0700

    The server now prints the client message

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var app = require("express")();
 var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
+  });
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
 });
 
 http.listen(3000, () => {
