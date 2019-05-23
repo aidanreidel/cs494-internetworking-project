847fb7704a2995e9a762cc1d591b4c5b6b927edb
Aidan Reidel <aidan.reidel@gmail.com>
2019-05-23 12:59:32 -0700

    Users can send and recieve messages!

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ app.get("/", (req, res) => {
 
 io.on("connection", function(socket) {
   console.log("a user connected");
+  socket.broadcast.emit("hi");
+
   socket.on("chat message", function(msg) {
-    console.log("message: " + msg);
+    io.emit("chat message", msg);
   });
+
   socket.on("disconnect", function() {
     console.log("user disconnected");
   });
