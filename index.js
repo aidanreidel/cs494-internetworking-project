75aba600b39360e90705b07e22f4d919d9a861b9
Aidan Reidel <aidan.reidel@gmail.com>
2019-06-04 13:33:52 -0700

    online users is now a list on the side

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ io.on("connection", function(socket) {
   socket.on("adduser", username => {
     usernames[username] = username;
     socket.username = username;
-    socket.room = 'room1';     // store room name in the socket session for this client
-    socket.join('room1');     // send client to room 1
+    socket.room = "room1"; // store room name in the socket session for this client
+    socket.join("room1"); // send client to room 1
     console.log(usernames);
     socket.emit("chat message", "SERVER", username + " joined room 1!"); // echo to client that that have joined
-    socket.broadcast.to("room1").emit("chat message", "SERVER", username + " has connected to this room");
+    socket.broadcast
+      .to("room1")
+      .emit("chat message", "SERVER", username + " has connected to this room");
+    io.emit("update user list", usernames); // This sends the user list over to the client
   });
 
   socket.on("chat message", function(msg) {
@@ -31,8 +34,12 @@ io.on("connection", function(socket) {
     fn(usernames);
   });
   socket.on("disconnect", function() {
-		io.sockets.emit("update users", usernames);            // update list of users in chat, client-side
-		socket.broadcast.emit("chat message", "SERVER", socket.username + ' has disconnected.'); 		// echo globally that this client has left
+    io.sockets.emit("update users", usernames); // update list of users in chat, client-side
+    socket.broadcast.emit(
+      "chat message",
+      "SERVER",
+      socket.username + " has disconnected."
+    ); // echo globally that this client has left
     socket.leave(socket.room);
     console.log("user disconnected");
   });
